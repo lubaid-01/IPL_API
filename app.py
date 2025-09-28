f47498cc48132e762af31b43697cc8faae6b2009
adnanbhat7 <adnanbhat70922@gmail.com>
2025-09-28 10:59:58 +0530

    added Query and Path parameters

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Request
+from fastapi import FastAPI, Request, Path , Query
 from fastapi.responses import JSONResponse, HTMLResponse
 from fastapi.templating import Jinja2Templates
 from fastapi.staticfiles import StaticFiles
@@ -23,39 +23,48 @@ async def main(request: Request):
 
 @app.get("/info")
 async def info_func():
-    return JSONResponse(content=info())
+    return info()
 
 # Team API
 @app.get("/team/{name}")
-async def t_func(name: str):
-    return JSONResponse(content=team_info(name.lower()))
+async def team_stats(name: str = Path(... , description= "Enter Team Name" ,example="Royal Challengers Bangalore")):
+    return team_info(name.lower())
 
 # Match API
-@app.get("/vs/{t1}/{t2}")
-async def m_func(t1: str, t2: str):
-    return JSONResponse(content=find_matches(t1.lower(), t2.lower()))
+@app.get("/vs")
+async def get_matchup(
+    team1: str = Query(..., description="Enter first team name", example="Delhi Daredevils"),
+    team2: str = Query(..., description="Enter second team name", example="Rajasthan Royals"),
+):
+    return find_matches(team1.lower(), team2.lower())
 
 # Player stats API
 @app.get("/player/{name}")
-async def p_func(name: str):
-    return JSONResponse(content=player_stats(name.lower()))
+async def get_player_stats(name: str = Path(...  , description= " Enter Player Name " , example= "V Kohli")):
+    return player_stats(name.lower())
 
 # Player vs Player API
-@app.get("/pvp/{batsman}/{bowler}")
-async def pvp_func(batsman: str, bowler: str):
-    return JSONResponse(content=pvp(batsman.lower(), bowler.lower()))
+@app.get("/pvp")
+async def get_player_matchhup(
+    batsman: str = Query(... , description="Enter Batsmen Name " , example = "V Kohli" ),
+    bowler: str = Query(... , description= "Enter Bowler Name " , example = "JJ Bumrah")
+):
+    return pvp(batsman.lower(), bowler.lower())
 
 # Player Venue API
-@app.get("/playervenue/{player}/{venue}")
-async def p_venue_func(player: str, venue: str):
-    return JSONResponse(content=player_stats(player_name=player.lower(), venue=venue.lower()))
+@app.get("/playervenue")
+async def player_in_venue_stats(
+    player: str = Query(... , description = "Enter Player Name " , example = "V Kohli"), 
+    venue: str = Query(... , description ="Enter Venue " , example = "m chinnaswamy stadium")
+):
+    return player_stats(player_name=player.lower(), venue=venue.lower())
 
 # Venue API
 @app.get("/venue/{name}")
-async def venue_fun(name: str):
-    return JSONResponse(content=ven_info(name.lower()))
+async def venue_stats(name: str = Path(... , description = "Enter Venue Name " , example = "m chinnaswamy stadium")):
+    return ven_info(name.lower())
 
 # Season API
 @app.get("/season/{name}")
-async def season_fun(name: str):
-    return JSONResponse(content=season_info(name))
+async def season_stats(name: str = Path(... , description = "Enter Season " , example ="2019" )):
+    return season_info(name)
